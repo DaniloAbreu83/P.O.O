309fa638757221af7a1dbbf622b004c5ef13b798
Danilo Abreu <danilo13abreu@gmail.com>
2026-05-01 23:01:40 -0300

    Ajuste no case 3 e implementando o case 4.

--- sistema_biblioteca/src/main/java/br/edu/ifpb/biblioteca/app/Main.java
+++ sistema_biblioteca/src/main/java/br/edu/ifpb/biblioteca/app/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import br.edu.ifpb.biblioteca.model.Usuario;
 import br.edu.ifpb.biblioteca.model.Livro;
 import br.edu.ifpb.biblioteca.service.BibliotecaService;
+import br.edu.ifpb.biblioteca.service.EmprestimoService;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,77 +21,88 @@ public class Main {
             System.out.println("3. Realizar emprestimo");
             System.out.println("4. Devolver livro");
             System.out.println("5. Sair");
-            
+
             System.out.print("Escolha: ");
             opcao = sc.nextInt();
             sc.nextLine();
-        
 
-        switch (opcao) {
-            case 1:
-                System.out.print("ID: ");
-                int id = sc.nextInt();
-                sc.nextLine();
+            switch (opcao) {
+                case 1:
+                    System.out.print("ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
 
-                System.out.print("Nome: ");
-                String nome = sc.nextLine();
-                
-                System.out.print("Tipo (ALUNO/PROFESSOR/FUNCIONARIO): ");
-                String tipo = sc.nextLine();
+                    System.out.print("Nome: ");
+                    String nome = sc.nextLine();
 
-                Usuario usuario = new Usuario(id, nome, tipo, 0, false);
-                service.adicionarUsuario(usuario);
+                    System.out.print("Tipo (ALUNO/PROFESSOR/FUNCIONARIO): ");
+                    String tipo = sc.nextLine();
 
-                System.out.println("Usuário adicionado com sucesso!");
-                break;
+                    Usuario usuario = new Usuario(id, nome, tipo, 0, false);
+                    service.adicionarUsuario(usuario);
 
-            case 2:
-                System.out.print("ISBN:");
-                String isbn = sc.nextLine();
+                    System.out.println("Usuário adicionado com sucesso!");
+                    break;
 
-                System.out.print("Titulo: ");
-                String titulo = sc.nextLine();
+                case 2:
+                    System.out.print("ISBN:");
+                    String isbn = sc.nextLine();
 
-                System.out.print("Autor: ");
-                String autor = sc.nextLine();
+                    System.out.print("Titulo: ");
+                    String titulo = sc.nextLine();
 
-                System.out.print("Editora: ");
-                String editora = sc.nextLine();
+                    System.out.print("Autor: ");
+                    String autor = sc.nextLine();
 
-                System.out.print("Ano:");
-                int ano = sc.nextInt();
-                sc.nextLine();
+                    System.out.print("Editora: ");
+                    String editora = sc.nextLine();
 
-                System.out.print("Paginas:");
-                int paginas = sc.nextInt();
-                sc.nextLine();
+                    System.out.print("Ano:");
+                    int ano = sc.nextInt();
+                    sc.nextLine();
 
-                Livro livro = new Livro(isbn, titulo, autor, editora, ano, paginas);
-                service.adicionarLivro(livro);
-                System.out.println("Livro adicionado com sucesso!");
-                break;
-            
-            case 3:
-                System.out.print("ID do usuário: ");
-                int idDev = sc.nextInt();
-                sc.nextLine();
+                    System.out.print("Paginas:");
+                    int paginas = sc.nextInt();
+                    sc.nextLine();
 
-                System.out.print("Título do livro: ");
-                String tituloDev = sc.nextLine();
+                    Livro livro = new Livro(isbn, titulo, autor, editora, ano, paginas);
+                    service.adicionarLivro(livro);
+                    System.out.println("Livro adicionado com sucesso!");
+                    break;
 
-                System.out.print("Dias de atraso: ");
-                int diasAtraso = sc.nextInt();
+                case 3:
+                    System.out.print("ID do usuário: ");
+                    int idUsuario = sc.nextInt();
+                    sc.nextLine();
 
-                service.registrarDevolucao(idDev, tituloDev, diasAtraso);
-                break;
+                    System.out.print("Título do livro: ");
+                    String tituloItem = sc.nextLine();
 
-            case 5:
-                System.out.println("Sair...");
-                break;
+                    service.realizarEmprestimo(idUsuario, tituloItem);
+                    break;
 
-            default:
-                System.out.println("Opção inválida. Por favor, tente novamente.");
-        }
+                case 4:
+                    System.out.print("ID do usuário: ");
+                    int idDev = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Título do livro: ");
+                    String tituloDev = sc.nextLine();
+
+                    System.out.print("Dias de atraso: ");
+                    int diasAtraso = sc.nextInt();
+                    sc.nextLine();
+
+                    service.registrarDevolucao(idDev, tituloDev, diasAtraso);
+                    break;
+
+                case 5:
+                    System.out.println("Sair...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Por favor, tente novamente.");
+            }
         }
 
     }
